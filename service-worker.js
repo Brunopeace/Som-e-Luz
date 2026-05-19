41754aa17f1985dcdf23007b3686e6ee01357f1f
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-05-19 11:48:32 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,14 @@
-const CACHE_NAME = 'som-e-luz-v3';
+const CACHE_NAME = 'som-e-luz-v29';
 const ASSETS = [
-  './',
-  './index.html',
-  './css/estilo.css',
-  './javascript/script.js',
-  './manifest.json',
-  './img/icon-192.png',
-  './img/icon-512.png',
-  './img/ambienteexclusivoesofisticado.jpg',
-  './img/somprofissionalcomdea.jpg',
-  './img/suafestabrilhacomnossapistadeled.jpg'
+  'index.html',
+  'css/style.css',
+  'js/script.js',
+  'manifest.json',
+  'img/icon-192.png',
+  'img/icon-512.png',
+  'img/ambienteexclusivoesofisticado.jpg',
+  'img/somprofissionalcomdea.jpg',
+  'img/suafestabrilhacomnossapistadeled.jpg'
 ];
 
 // Instala o Service Worker e guarda os arquivos essenciais no cache
@@ -17,7 +16,7 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting()) // Força o novo service worker a se tornar o ativo imediatamente
   );
 });
 
@@ -32,7 +31,7 @@ self.addEventListener('activate', (e) => {
           }
         })
       );
-    })
+    }).then(() => self.clients.claim()) // Garante que as abas atuais usem o sw corrigido imediatamente
   );
 });
 
