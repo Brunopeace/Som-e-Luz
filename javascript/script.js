ac80a59b8d78d6cf2bb4e7cafc6385bd4b846fd3
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-05-19 22:34:27 -0300

    Atualizar o script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 // REGISTRO DO SERVICE WORKER PARA O PWA
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(reg => console.log('✅ Service Worker registrado com sucesso!', reg))
             .catch(err => console.error('Erro ao registrar o Service Worker:', err));
     });
